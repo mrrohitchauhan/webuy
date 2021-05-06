300e64f8a0a07f1e246cf78be4ab0a800d833293
Rohit Chauhan <34659271+mrrohitchauhan@users.noreply.github.com>
2021-05-07 02:05:24 +0530

    final commit

--- webut-admin/src/environments/environment.ts
+++ webut-admin/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCcGEZObEIFMaJ4lpORJSZLA2qTDcjKKFk',
+    authDomain: 'ecommerce-42a66.firebaseapp.com',
+    projectId: 'ecommerce-42a66',
+    databaseURL: 'https://ecommerce-42a66-default-rtdb.firebaseio.com/',
+    storageBucket: 'ecommerce-42a66.appspot.com',
+    messagingSenderId: '432478962321',
+    appId: '1:432478962321:web:78d1cbdfd9b24eff0df530',
+    measurementId: 'G-L5VX31RVZY',
+  },
 };
 
 /*
--- webuy-client/src/environments/environment.ts
+++ webuy-client/src/environments/environment.ts
@@ -5,13 +5,21 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyATLKpanc-xpOSQgj6aOB3K4XHI84J9ICg",
-    authDomain: "webuy-804c5.firebaseapp.com",
-    projectId: "webuy-804c5",
-    storageBucket: "webuy-804c5.appspot.com",
-    messagingSenderId: "366395148231",
-    appId: "1:366395148231:web:4807c1b55f73f8727fbbe2",
-    databaseURL: 'https://webuy-804c5-default-rtdb.firebaseio.com/',
+    // apiKey: "AIzaSyATLKpanc-xpOSQgj6aOB3K4XHI84J9ICg",
+    // authDomain: "webuy-804c5.firebaseapp.com",
+    // projectId: "webuy-804c5",
+    // storageBucket: "webuy-804c5.appspot.com",
+    // messagingSenderId: "366395148231",
+    // appId: "1:366395148231:web:4807c1b55f73f8727fbbe2",
+    // databaseURL: 'https://webuy-804c5-default-rtdb.firebaseio.com/',
+    apiKey: 'AIzaSyCcGEZObEIFMaJ4lpORJSZLA2qTDcjKKFk',
+    authDomain: 'ecommerce-42a66.firebaseapp.com',
+    projectId: 'ecommerce-42a66',
+    databaseURL: 'https://ecommerce-42a66-default-rtdb.firebaseio.com/',
+    storageBucket: 'ecommerce-42a66.appspot.com',
+    messagingSenderId: '432478962321',
+    appId: '1:432478962321:web:78d1cbdfd9b24eff0df530',
+    measurementId: 'G-L5VX31RVZY',
   },
 };
 
